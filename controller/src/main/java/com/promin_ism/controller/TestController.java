a74bb2114b30dca5104d841d57c8b7e36463e3a7
yaroslav kobylsky <yaroslaff.k@gmail.com>
2016-10-17 22:20:01 +0300

    added log4j dependency and configuration

--- controller/src/main/java/com/promin_ism/controller/TestController.java
+++ controller/src/main/java/com/promin_ism/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.promin_ism.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
+    private static final Logger LOGGER = Logger.getLogger(TestController.class);
+
 
     @RequestMapping("/test")
     public ModelAndView testPageHelloWorld() {
@@ -18,7 +21,7 @@ public class TestController {
     @RequestMapping(value = "/testAJAX", method = RequestMethod.GET)
     @ResponseBody
     public  String testAjax(){
-        System.out.println("test ajax controller");
+        LOGGER.debug("test ajax controller");
         return "json view resolver welcomes  you!";
     }
 }
